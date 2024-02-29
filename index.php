aa4936a005c0f74d1ae6398e2b7e96770431a4c0
josias <18200009@liberato.com.br>
2024-02-28 23:53:50 -0300

    Criação do projeto

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <link rel="icon" type="image/x-icon" href="img/praise_icon.ico" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Comunidade PraiseLAB</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<style>
+  a{
+    color: #fff;
+  }
+
+  a:hover {
+    color: #c2cbe5;
+  }
+</style>
+<body>
+
+  <nav class="navbar navbar-expand navbar-dark bg-dark topbar static-top shadow">
+
+    <img src="img/logo.png" alt="logo" style="width: 8%;">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <a style="text-decoration: none;" href="#"><b>Início</b></a>
+        </div>
+        <div class="col-4">
+          <a style="text-decoration: none;" href="#"><b>Comunidades</b></a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Topbar Navbar -->
+    <ul class="navbar-nav ml-auto">
+
+      <li class="nav-item dropdown p-1">
+        <input class="form-control form-control-user" type="text" style="border-color: #292F35 !important; background-color: #3E454E !important;"  placeholder="Procurar...">
+      </li>
+      <li class="nav-item dropdown p-1">
+        <button class="btn btn-secondary ">Login</button>
+      </li>
+
+    </ul>
+
+  </nav>
+
+  <div class="bg">
+    <img src="img/bmp_top.png" alt="Foto do projeto">
+  </div>
+  <div class="bg-dark">
+    
+  </div>
+  
+</body>
+</html>
