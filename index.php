1315758d6d6b079b8e5650e985f3054e1d0d51dd
JosiasWeberMachado <weberjosias1@gmail.com>
2024-02-29 17:26:26 -0300

    implementa a lib de icones e inicia a formação da primeira section

--- index.php
+++ index.php
@@ -9,16 +9,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Comunidade PraiseLAB</title>
   <link rel="stylesheet" href="css/style.css">
-</head>
-<style>
-  a{
-    color: #fff;
-  }
+  <link rel="stylesheet" href="libs/fontawesome/css/all.css">
+  <style>
+    ::-webkit-scrollbar {
+      background-color: #292F35; /* Track background color */
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: #3E454E; /* Thumb background color */
+    }
 
-  a:hover {
-    color: #c2cbe5;
-  }
-</style>
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: #999; /* Change color on hover */
+    }
+    body{
+      overflow-x: hidden;
+    }
+    a{
+      color: #fff;
+    }
+    a:hover {
+      color: #c2cbe5;
+    }
+
+  </style>
+</head>
+
 <body>
 
   <nav class="navbar navbar-expand navbar-dark bg-dark topbar static-top shadow">
@@ -40,7 +55,7 @@
     <ul class="navbar-nav ml-auto">
 
       <li class="nav-item dropdown p-1">
-        <input class="form-control form-control-user" type="text" style="border-color: #292F35 !important; background-color: #3E454E !important;"  placeholder="Procurar...">
+        <input class="form-control form-control-user shadow" type="text" style="border-color: #292F35 !important; background-color: #3E454E !important;"  placeholder="Procurar...">
       </li>
       <li class="nav-item dropdown p-1">
         <button class="btn btn-secondary ">Login</button>
@@ -50,12 +65,60 @@
 
   </nav>
 
-  <div class="bg">
+  <section class="bg">
     <img src="img/bmp_top.png" alt="Foto do projeto">
-  </div>
-  <div class="bg-dark">
-    
-  </div>
+  </section>
+  <section class="bg-dark">
+
+    <div class="container-fluid">
+        <div class="row">
+          <div class="ml-5 pt-3 pl-4">
+            <h3 style="color:#fff;"><i class="fa-solid fa-bars"></i> <b>Conteúdos</b></h3>
+          </div>
+        </div>
+      
+        <div class="row justify-content-center">
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card1.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-6">
+                <h5><b style="color:#fff;">auLIVEs</b></h5>
+              </div>
+              <div class="text-right"><b style="color:#fff;">Lives</b></div>
+            </div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card2.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card3.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card4.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card5.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card6.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card7.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card8.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card9.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+          <div class="col-md-2 m-4">
+            <div class="icon-box bg p-6" style="background:url(img/card10.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+      </div>
+
+    </div>
+
+  </section>
   
 </body>
 </html>
