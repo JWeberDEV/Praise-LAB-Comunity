d1935625962770569358f94ac81a5077993e8316
JosiasWeberMachado <weberjosias1@gmail.com>
2024-03-14 17:25:30 -0300

    continua a criação do index

--- index.php
+++ index.php
@@ -53,10 +53,6 @@
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
-
-      <li class="nav-item dropdown p-1">
-        <input class="form-control form-control-user shadow" type="text" style="border-color: #292F35 !important; background-color: #3E454E !important;"  placeholder="Procurar...">
-      </li>
       <li class="nav-item dropdown p-1">
         <button class="btn btn-secondary ">Login</button>
       </li>
@@ -66,9 +62,10 @@
   </nav>
 
   <section class="bg">
-    <img src="img/bmp_top.png" alt="Foto do projeto">
+    <img src="img/bmp_top.png" style="width: 100%;" alt="Foto do projeto">
   </section>
-  <section class="bg-dark">
+
+  <section class="bg-high-dark">
 
     <div class="container-fluid">
         <div class="row">
@@ -81,43 +78,212 @@
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card1.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
             <div class="row pt-2">
-              <div class="col-6">
+              <div class="col-9">
                 <h5><b style="color:#fff;">auLIVEs</b></h5>
               </div>
-              <div class="text-right"><b style="color:#fff;">Lives</b></div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Lives</b></span>  
+              </div>
             </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card2.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Repertório Express</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Séries</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card3.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Arcordes</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Seção</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card4.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Desafio Pestaneiro</b></h5>
+              </div>
+              <div class="">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Desafio</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card5.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Laboratório de Louvor</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Série</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card6.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Repertórios</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Série</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card7.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+              <h5><b style="color:#fff;">5 Arcordes e 30 Louvores</b></h5>
+              </div>
+              <div>
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">E-book</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card8.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Do Zero ao Violeiro</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Curso</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card9.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Lives com Guto Ail</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Lives</b></span>  
+              </div>
+            </div>
           </div>
           <div class="col-md-2 m-4">
             <div class="icon-box bg p-6" style="background:url(img/card10.png); background-size: 100%; background-position: center; background-repeat: no-repeat;"></div>
+            <div class="row pt-2">
+              <div class="col-9">
+                <h5><b style="color:#fff;">Cifras do Canal do Guto Ail</b></h5>
+              </div>
+              <div class="col-2">
+                <span class="badge bg-secondary" style="border-radius: 10px !important;"><b style="font-size: 16px;">Cifras</b></span>  
+              </div>
+            </div>
           </div>
       </div>
 
     </div>
 
+  </section>
+
+  <section class="bg-high-dark pb-4 pt-4">
+    <div class="row justify-content-center">
+      <span class="inline-flex w-16 px-1 py-1 bg-secondary rounded-pill" aria-hidden="true" style="width:100px;"></span>
+    </div>
+  </section>
+
+  <section class="bg-high-dark">
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </section>
+
+  <section class="bg-high-dark pt-5">
+    <div class="container">
+      <div class="d-flex text-center justify-content-evenly pb-5">
+        <div class="col-4">
+          <h1><span class="badge rounded-circle p-3 bg-danger"><i class="fa-solid fa-clock"></i></span></h1>
+            <h4><b>Conteúdo online</b></h4>
+            Acesse o clube de qualquer lugar sempre que quiser.
+        </div>
+        <div class="col-4">
+          <h1><span class="badge rounded-circle p-3 bg-danger"><i class="fa-solid fa-clock"></i></span></h1>
+          <h4><b>Acesso imediato</b></h4>
+          Receba seu acesso imediatamente após a compra.
+        </div>
+        <div class="col-4">
+          <h1><span class="badge rounded-circle p-3 bg-danger"><i class="fa-solid fa-clock"></i></span></h1>
+          <h4><b>Garantia total</b></h4>
+          Você tem uma garantia de 7 dias de satisfação total.
+        </div>
+      </div>
+      <div class="p-6 border border-2 border-ligh rounded rounded-5">
+        <h1 class="text-center pb-3"><b>Comunidade PraiseLAB</b></h1>
+        <p class="text-center" style="font-size: 1.40rem; color: rgb(107, 114, 128);">Uma comunidade recheada de conteúdos, cifras, dicas, cursos, desafios, tudo isso com o suporte de dois professores, Guto Ail e Tiago Palma. Conteúdos para quem quer aprender a tocar, ampliar o repertório, conteúdos mais complexos, estudar arranjos, cifras das músicas e suporte para o Ministério da Música.</p>
+        <div class="d-grid gap-2 col-6 mx-auto pt-3 text-center">
+          <button class="btn btn-secondary btn-lg" type="button"><b>Faça parte da nossa comunidade!</b></button>
+        </div>
+        <p class="p-2"> </p>
+        <ul class="d-flex justify-content-evenly" style="color: rgb(107, 114, 128); list-style-type: none;">
+          <li>Acesso imediato</li>
+          <li>•</li>
+          <li>Garantia de 7 dias</li>
+          <li>•</li>
+          <li>Ajuda e suporte</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-high-dark pb-5 pt-5">
+    <div class="row justify-content-center">
+      <span class="inline-flex w-16 px-1 py-1 bg-secondary rounded-pill" aria-hidden="true" style="width:100px;"></span>
+    </div>
+  </section>
+
+  <section class="bg-high-dark">
+    <div class="container">
+      <div class="d-flex text-center justify-content-evenly pb-5">
+        <div class="col-4">
+          <h1><i class="fa-solid fa-clock"></i></h1>
+            <h4><b>C100% seguro</b></h4>
+            Pague com segurança e mantenha sua privacidade protegida.
+        </div>
+        <div class="col-4">
+          <h1><i class="fa-solid fa-clock"></i></h1>
+          <h4><b>Onde estiver</b></h4>
+          Acesse o conteúdo do clube através de qualquer dispositivo.
+        </div>
+        <div class="col-4">
+          <h1><i class="fa-solid fa-clock"></i></h1>
+          <h4><b>Com ajuda</b></h4>
+          Tire dúvidas e receba suporte através do email.
+        </div>
+      </div>
+    </div>
   </section>
   
 </body>
